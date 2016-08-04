5013e7af52615627639d32f3ad3d8f1db6cb87b1
Artem Gordienko <arking@ukr.net>
2016-08-04 09:53:46 +0300

    Add files via upload

--- initdb.php
+++ initdb.php
@@ -1,12 +1,14 @@
 <?php
 include ('db.php');
-$sql='create table users(
+$sql='
+drop table users;
+create table users(
 id  SERIAL NOT NULL  PRIMARY KEY,
-login VARCHAR (50),
-email VARCHAR (50),
-last_name VARCHAR (50),
-first_name VARCHAR (50),
-age int,
-password VARCHAR (100)
+login VARCHAR (50) UNIQUE NOT NULL, 
+email VARCHAR (50) UNIQUE NOT NULL,
+last_name VARCHAR (50) NOT NULL,
+first_name VARCHAR (50) NOT NULL,
+age int NOT NULL,
+password VARCHAR (100) NOT NULL
 )';
 $db->exec($sql);
